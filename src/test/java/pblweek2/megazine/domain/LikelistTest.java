b7b356df64570ab14783773a9d650ab9b4921ecb
Shinnybest <kim295521@naver.com>
2022-02-28 22:40:54 +0900

    [FEAT] Likelist TESTCODE

--- src/test/java/pblweek2/megazine/domain/LikelistTest.java
+++ src/test/java/pblweek2/megazine/domain/LikelistTest.java
@@ -1,18 +1,54 @@
 package pblweek2.megazine.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import pblweek2.megazine.repository.BoardRepository;
+import pblweek2.megazine.repository.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class LikelistTest {
-//    @Test
-//    @DisplayName("setUser 테스트")
-//    void setUsertoLikelists() {
-//        //given
-//
-//        //when
-//        //then
-//    }
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("좋아요 생성")
+    void like() {
+        //given
+        User user = User.builder()
+                .username("kitty")
+                .email("hello@gmail.com")
+                .password("1234")
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        Board board = Board.builder()
+                .grid("1234")
+                .content("Bigbang")
+                .username("kitty")
+                .imageUrl("www.image.com")
+                .build();
+
+        Board savedBoard = boardRepository.save(board);
+
+        //when
+        Likelist likelist = Likelist.builder()
+                .user(savedUser)
+                .board(savedBoard)
+                .build();
+
+        //then
+        Assertions.assertEquals(savedUser.getId(), likelist.getUser().getId());
+        Assertions.assertEquals(savedBoard.getId(), likelist.getBoard().getId());
+    }
 }
