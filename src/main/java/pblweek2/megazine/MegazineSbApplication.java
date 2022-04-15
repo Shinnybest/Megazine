76d40e18bc8f61798537fd0c7cb4378f4ad227cb
Shinnybest <kim295521@naver.com>
2022-04-15 11:14:13 +0900

    [CHORE] application.yml 파일 추가

--- src/main/java/pblweek2/megazine/MegazineSbApplication.java
+++ src/main/java/pblweek2/megazine/MegazineSbApplication.java
@@ -11,5 +11,4 @@ public class MegazineSbApplication {
     public static void main(String[] args) {
         SpringApplication.run(MegazineSbApplication.class, args);
     }
-
 }
--- src/main/java/pblweek2/megazine/repository/UserRepository.java
+++ src/main/java/pblweek2/megazine/repository/UserRepository.java
@@ -1,10 +1,6 @@
 package pblweek2.megazine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import pblweek2.megazine.domain.User;
 
 import java.util.Optional;
